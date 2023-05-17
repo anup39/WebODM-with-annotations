433f331f9d1405f43e4ac9de315331473adf0247
Anup Dahal <adahal39@gmail.com>
2023-05-17 08:39:43 +0545

    feat: popup generating function moved to new file

--- app/static/app/js/components/Map.jsx
+++ app/static/app/js/components/Map.jsx
@@ -33,60 +33,7 @@ import { _ } from "../classes/gettext";
 // # ADDED BY ME
 import "leaflet-draw/dist/leaflet.draw.css";
 import "leaflet-draw";
-
-function generateForm(
-  categories,
-  deleteSelectedCategory,
-  editSelectedCategory,
-  saveSelectedCategory
-) {
-  const form = document.createElement("form");
-
-  const message = document.createElement("p");
-  message.textContent = "Please select a category to save.";
-  message.classList.add("error-message");
-  form.appendChild(message);
-
-  const categoryGroup = document.createElement("div");
-
-  categories.forEach((category) => {
-    const label = document.createElement("label");
-    label.textContent = category.category;
-
-    const radio = document.createElement("input");
-    radio.type = "radio";
-    radio.name = "selectedCategory";
-    radio.value = category.id;
-
-    label.appendChild(radio);
-    categoryGroup.appendChild(label);
-    categoryGroup.appendChild(document.createElement("br"));
-  });
-
-  form.appendChild(categoryGroup);
-
-  const saveButton = document.createElement("button");
-  saveButton.textContent = "Save";
-  saveButton.type = "submit";
-
-  const deleteButton = document.createElement("button");
-  deleteButton.textContent = "Delete";
-  deleteButton.type = "button";
-  deleteButton.addEventListener("click", deleteSelectedCategory);
-
-  const editButton = document.createElement("button");
-  editButton.textContent = "Edit";
-  editButton.type = "button";
-  editButton.addEventListener("click", editSelectedCategory);
-
-  form.appendChild(saveButton);
-  form.appendChild(deleteButton);
-  form.appendChild(editButton);
-
-  form.addEventListener("submit", saveSelectedCategory);
-
-  return form;
-}
+import mapPopupGenerator from "./MapPopupGenerator";
 
 class Map extends React.Component {
   static defaultProps = {
@@ -808,7 +755,7 @@ class Map extends React.Component {
 
       // layer.bindPopup(popupContent).openPopup();
       layer.bindPopup(
-        generateForm(
+        mapPopupGenerator(
           categories,
           deleteSelectedCategory,
           editSelectedCategory,
---
+++ app/static/app/js/components/MapPopupGenerator.jsx
@@ -0,0 +1,55 @@
+const  mapPopupGenerator(
+  categories,
+  deleteSelectedCategory,
+  editSelectedCategory,
+  saveSelectedCategory
+) =>{
+  const form = document.createElement("form");
+
+  const message = document.createElement("p");
+  message.textContent = "Please select a category to save.";
+  message.classList.add("error-message");
+  form.appendChild(message);
+
+  const categoryGroup = document.createElement("div");
+
+  categories.forEach((category) => {
+    const label = document.createElement("label");
+    label.textContent = category.category;
+
+    const radio = document.createElement("input");
+    radio.type = "radio";
+    radio.name = "selectedCategory";
+    radio.value = category.id;
+
+    label.appendChild(radio);
+    categoryGroup.appendChild(label);
+    categoryGroup.appendChild(document.createElement("br"));
+  });
+
+  form.appendChild(categoryGroup);
+
+  const saveButton = document.createElement("button");
+  saveButton.textContent = "Save";
+  saveButton.type = "submit";
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.type = "button";
+  deleteButton.addEventListener("click", deleteSelectedCategory);
+
+  const editButton = document.createElement("button");
+  editButton.textContent = "Edit";
+  editButton.type = "button";
+  editButton.addEventListener("click", editSelectedCategory);
+
+  form.appendChild(saveButton);
+  form.appendChild(deleteButton);
+  form.appendChild(editButton);
+
+  form.addEventListener("submit", saveSelectedCategory);
+
+  return form;
+}
+
+export default  mapPopupGenerator
