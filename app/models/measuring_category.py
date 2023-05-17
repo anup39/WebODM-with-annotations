f264630feb5bc9b988a827eba19333741949fe23
Anup Dahal <adahal39@gmail.com>
2023-05-17 15:55:24 +0545

    feat: Measuring Category model initiated

---
+++ app/models/measuring_category.py
@@ -0,0 +1,21 @@
+from .project import Project
+from django.contrib.gis.db.models.fields import GeometryField
+from django.db import models
+
+
+class MeasuringCategory(models.Model):
+    name = models.CharField(max_length=255, help_text=_(
+        "In which category you want to seperate your project layer"), verbose_name=_("Name"))
+    project = models.ForeignKey(Project, on_delete=models.PROTECT, help_text=_(
+        "Category related to the project"), verbose_name=_("Project"))
+    description = models.TextField(default="", blank=True, help_text=_(
+        "Description about this category"), verbose_name=_("Description"))
+    created_at = models.DateTimeField(default=timezone.now, help_text=_(
+        "Creation date"), verbose_name=_("Created at"))
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _("MeasuringCategory")
+        verbose_name_plural = _("MeasuringCategories")
