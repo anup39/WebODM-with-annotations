2d32ade0ae3388de05b2da6f43abc3b8911a3047
Anup Dahal <adahal39@gmail.com>
2023-05-26 19:03:22 +0545

    Added filter so that category can be filtered through project id

--- app/api/measuring_category.py
+++ app/api/measuring_category.py
@@ -1,5 +1,16 @@
 from rest_framework import serializers, viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 from app import models
+import django_filters
+from rest_framework import filters
+
+
+class MeasuringCategoryFilter(django_filters.FilterSet):
+    project = django_filters.CharFilter(field_name='project__id')
+
+    class Meta:
+        model = models.MeasuringCategory
+        fields = ['project']
 
 
 class MeasuringCategorySerializer(serializers.ModelSerializer):
@@ -13,3 +24,6 @@ class MeasuringCategoryViewSet(viewsets.ModelViewSet):
 
     queryset = models.MeasuringCategory.objects.all()
     serializer_class = MeasuringCategorySerializer
+    filter_backends = [DjangoFilterBackend,
+                       filters.SearchFilter, filters.OrderingFilter]
+    filter_class = MeasuringCategoryFilter
